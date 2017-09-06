9f04290140b8da2540009d9776f797b0f176b1dc
KRSMACE <khallid.martin@f1006-29-34175.bmcc.cuny.edu>
2017-09-05 20:29:16 -0400

    initial commit

---
+++ selfportrait/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(640, 460)
+}
+function draw() {
+    background("white");
+    
+    noStroke()
+    fill("saddlebrown")
+    ellipse(155,200,200,250);
+    
+    fill("black")
+    ellipse(105,200,100,50);
+    ellipse(205,200,100,50);
+    
+    fill("white")
+    ellipse(125,200,55,55);
+    ellipse(225,200,55,55);
+    
+    
+    
+    
+}
+
+/*function draw() {
+    background("white")
+    
+    ellispe(100,100,100,100);
+}
+  */
