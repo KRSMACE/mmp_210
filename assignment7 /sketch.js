53ca69bd6eba28853928b5bf04123cfe4be28a1a
KRSMACE <khallid.martin1@gmail.com>
2017-11-28 18:16:00 -0500

    assign7 comp

--- assignment7 /sketch.js
+++ assignment7 /sketch.js
@@ -1,58 +1,52 @@
+
 function setup() {
-    createCanvas(500, 500);
-    design();
-    //rectMode(CENTER);
-    noStroke();
+  var columnSize = 75;
+  var rowSize = 130;
+  var yellow = color(255, 248, 214);
+  var gry = color(105,105,105);
+  var brown = color(139, 69, 19);
+  var darkBrown = color(107, 58, 3);
+  var black = color(102, 51, 0);
     
-    
-}
+	  createCanvas(640, 480);
+    background (gry);
 
-function design() {
-    
-   background("black");
-    noStroke();
-    var numberOfRects = random(100,1000);
-    var columns = Math.ceil(Math.sqrt(numberOfRects));
-    var rows = columns;
-    var w = width/columns;
-    var h = height/rows;
-    
-    for (var x = 0; x < width; x += w) {
-        for (var y = 0; y < height; y += h) {
-            var c = random(0,255);
-            fill(c);
-            var r = random(100,170);
-            var g = random(0,70);
-            var b = random(0,100);
-            var sz = random();
-            fill(r, g, b);
-            //rect(x, y, w, w);
-            
-           var randomShape = random(0,4);
-            if ( randomShape < 1 ) {
-            // draw this shape...
-                rect(x, y, w, h);
-            }
-            else if ( randomShape < 2 ) {
-                //stroke(255);
-                triangle(x, y, x + w, y, x + w, y + h);
-            // draw this shape
-            }
-            else if (randomShape < 3) {
-                ellipse(x + w/2, y + h/2, w, h);
-            // draw this shape
-            }
-            else {
-                arc(x, y, w*2, h*2, 0, HALF_PI);  
-            }
-    
-           
-        }
+    for (var x = 0; x < width; x += columnSize) {
+      for (var y = 0; y < height; y += rowSize) {
+        //window
+        noStroke ();
+        fill(black);  
+        rect(x-27.5,y+90,55,10);  
+        fill(darkBrown);  
+        rect(x-32.5,y+85,65,10);
+        fill(darkBrown);  
+        rect(x-27.5,y+85,55,10);  
+        fill(brown);
+        ellipse(x,y,50);
+        rect(x-25,y,50,85);
+        //bottom WindRect
+        rect(x-35,y+75,70,15);
+       
+        
+        fill(yellow);
+        ellipse(x,y,40);
+        rect(x-20,y,40,80);
+        
+        fill(brown);
+        ellipse(x,y,8);
+        //Horizaontal  
+        rect(x-25,y,50,4);
+        rect(x-25,y+20,50,4);
+        rect(x-25,y+50,50,4);
+        //Vertical  
+        rect(x-15,y-20,4,20);
+        rect(x+11,y-20,4,20); 
+        rect(x-2,y-25,4,25);
+        
+        
+          
+        
     }
-    
-   
-
+  }
 }
-
-function mousePressed() {
-    design();
+  
