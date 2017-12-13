5b4f321902dbc9170d2f11151569f6794bcb8449
KRSMACE <khallid.martin1@gmail.com>
2017-12-12 21:55:50 -0500

    2nd to last class

--- sound practice/sketch.js
+++ sound practice/sketch.js
@@ -1,26 +1,38 @@
 var circles = [];
-var soundFiles = ["meme1.mp3", "meme2.mp3", "meme3.mp3"];
-var sounds = [];
+//var soundFiles = [ "meme2.mp3", "meme3.mp3", "meme1.mp3" ];
+//var sounds = [];
 
-/*function preload() {
-	for (var i = 0; i < soundFiles.length; i++) {
-		var s = loadSound(soundFiles[i]);
-		sounds.push(s);
-	}
-}*/
+// Unloaded sound files
+//var ytSounds = ["youtube/youtube0.mp3"];
+//Loaded sound files
+var youtube = [];
+var randomYtSound;
+
+function preload() {
+//	for (var i = 0; i < soundFiles.length; i++) {
+//		var s = loadSound(soundFiles[i]);
+//		sounds.push(s);
+//	}
+    
+    for (let i = 0; i < 3; i++) {
+        let s = loadSound("youtube/youtube" + i + ".mp3");
+        youtube.push(s);
+    }
+}
 
 function setup() {
 	createCanvas(720, 720);
 	noStroke();
+    console.log(youtube);
+    randomYtSound = floor(random(0,3));
 //	for (var i = 0; i < 10; i++) {
 //		var c = new Circle(random(0,width), random(0,height));
 //		circles.push(c);
 //	}
-	
-	var circle1 = new Circle(width/4, height/4,  sounds);
-	circles.push(circle1);
-	var circle2 = new Circle(width * 3/4, height/4,  sounds[1]);
-	circles.push(circle2);
+    
+    var ytCircle1 = new ytCircle(width/4, height/4, youtube[randomYtSound]);
+	circles.push(ytCircle1);
+    
 }
 
 function draw() {
@@ -28,22 +40,23 @@ function draw() {
 	for (var i = 0; i < circles.length; i++) {
 		circles[i].display();
 	}
-    
 }
 
 function mousePressed() {
+    randomYtSound = floor(random(0,3));
+    console.log();
 	for (var i = 0; i < circles.length; i++) {
 		circles[i].click();
 	}
 }
 
-function Circle(x, y, s) {
+function ytCircle(x, y, s) {
 	this.x = x;
 	this.y = y;
 	this.c = color( random(255), random(255), random(255));
 	this.size = random(50,100);
 	//this.sound = random(sounds);
-	this.sounds = s;
+	this.sound = s;
 	this.display = function() {
 		fill(this.c);
 		ellipse(this.x, this.y, this.size);
@@ -51,12 +64,9 @@ function Circle(x, y, s) {
 	this.click = function() {
 		var d = dist(mouseX, mouseY, this.x, this.y);
 		if ( d < this.size) {
-            //var s = random(this.sounds);
-            //s.play();
-            random(this.sounds).play();
 			this.c = color( random(255), random(255), random(255));
-			for (var i = 0; i < sounds.length; i++) {
-				sounds[i].stop();
+			for (var i = 0; i < youtube.length; i++) {
+				youtube[i].stop();
 			}
 			this.sound.play();
 		}
